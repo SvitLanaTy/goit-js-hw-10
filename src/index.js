4ffa5b816179813de37c49403451e127c002eea1
SvitLanaTy <stymchenko@gmail.com>
2023-05-08 20:11:39 +0200

    add refs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+import refs from './scripts/refs.js';
+import { fetchCountries } from './scripts/fetchCountries.js';
 
 const DEBOUNCE_DELAY = 300;
+console.log(refs.searchBoxEl);
---
+++ src/scripts/refs.js
@@ -0,0 +1,5 @@
+export default {
+  searchBoxEl: document.getElementById('search-box'),
+  countryListEl: document.querySelector(`.country-list`),
+  countryInfoEl: document.querySelector(`.country-info`),
+};
